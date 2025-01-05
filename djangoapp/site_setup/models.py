50b1dd4a9963b68286ed89347d38ecb19e64bb1b
cuesta0900 <pedropcs608@gmail.com>
2025-01-05 17:53:48 -0300

    favicon changes

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from utils.model_validators import validate_png
+from utils.images import resize_image
 
 # Create your models here.
 class MenuLink(models.Model):
@@ -30,6 +32,24 @@ class SiteSetup(models.Model):
     show_description = models.BooleanField(default=True)
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
+    
+    favicon = models.ImageField(
+        upload_to='assets/favicon/%Y/%m/',
+        blank=True, default='',
+        validators=[validate_png],
+    )
+    
+    def save(self, *args, **kwargs):
+        current_favicon_name = str(self.favicon.name) #Pega o nome do icon atual
+        super().save(args, kwargs) # salva
+
+        favicon_changed = False
+        if self.favicon: #Se tiver um favicon
+            favicon_changed = current_favicon_name != self.favicon.name #True se o icon foi mudado
+        
+        if favicon_changed: #se foi mudado faz o redimensionamento da imagem
+            resize_image(self.favicon, 32) #importado do images.py
+        
     
     def __str__(self):
         return self.title
