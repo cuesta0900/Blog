3604af9ae2932b3c34f571973efbc1d1d7d5e043
cuesta0900 <pedropcs608@gmail.com>
2025-02-03 22:52:46 -0300

    Tag view + partials _script

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('page/<slug:slug>/', blog.page, name='page'),
     path('created_by/<int:author_pk>/', blog.created_by, name='created_by'),
     path('category/<slug:slug>/', blog.category, name='category'),
+    path('tag/<slug:slug>/', blog.tag, name='tag'),
 ]
